635af12b1e05b4dbfc2a975b57ce0458b39d366c
Carmedine <carmedine@protonmail.com>
2025-09-25 15:58:05 +0200

    premier commit

---
+++ app.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+//import de express avec require
+const express= require('express');
+const mongoose = require('mongoose');
+const cors= require('cors');
+//appel de la methode express
+const app=express();
+
+
+app.use(cors());
+// donne acces au corps JSON de la requete aussi bodyParser
+app.use(express.json());
+
+//Connexion à MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+   // {useNewUrlParser: true,
+    //    useUnifiedTopology: true
+   // })
+    .then(()=>console.log ('connexion à MongoDB réussie!'))
+    .catch ((err)=> console.log ('connexion à MongoDB échouée!', err));
+
+
+
+//ajout des headers CORS
+//app.use ((req, res, next)=>{
+  //  res.setHeader ('Access-Control-Allow-Origin','*');
+  //  res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content,Accept,Content-Type,Authorization');
+  //  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  //  next();
+//});
+
+//Middlewaure pour log les requetes
+
+//route 
+app.post('/api/auth/signup',(req,res,next)=>{
+    console.log('données recues:',req.body);
+    res.status(201).json({
+        message:'identifiant crée!'
+    });
+});
+
+
+app.use((req,res,next)=>{
+    console.log('requete recue');
+    next();
+});
+
+
+
+app.use ((req,res)=> {
+    res.status(404).json({message:'route non trouvée'});
+    next();
+});
+
+
+
+module.exports=app;
